2f223b935a3f0871f936b5e0072a7cff430a10fb
Alaina Bartfeld <alainabartfeld@gmail.com>
2025-12-14 09:24:36 -0800

    answering more questions, asking further questions

--- strava_analysis.py
+++ strava_analysis.py
@@ -37,7 +37,7 @@ staging = duckdb.sql(f"""
         ,sport_type
         ,workout_type
         ,device_name
-        ,start_date as start_date_utc
+        ,start_date AS start_date_utc
         ,start_date_local
         ,timezone
         ,achievement_count
@@ -53,15 +53,15 @@ staging = duckdb.sql(f"""
         ,CASE
             WHEN LENGTH(CAST(month(start_date_local) AS VARCHAR))=1 THEN concat(year(start_date_local), '-0', month(start_date_local))
             ELSE concat(year(start_date_local), '-',  CAST(month(start_date_local) AS VARCHAR))
-            END as start_date_local_yyyy_mm
+            END AS start_date_local_yyyy_mm
     
         -- STANDARDIZED MEASURES
-        ,distance as distance_meters
-        ,moving_time as moving_time_secs
-        ,elapsed_time as elapsed_time_secs
-        ,elev_high as elev_high_meters
-        ,elev_low as elev_low_meters
-        ,total_elevation_gain as total_elevation_gain_meters
+        ,distance AS distance_meters
+        ,moving_time AS moving_time_secs
+        ,elapsed_time AS elapsed_time_secs
+        ,elev_high AS elev_high_meters
+        ,elev_low AS elev_low_meters
+        ,total_elevation_gain AS total_elevation_gain_meters
         ,round(distance / {miles_to_meters},2) AS distance_miles
         ,round(moving_time / {secs_to_mins},2) AS moving_time_mins
         ,round(moving_time / {secs_to_hrs},2) AS moving_time_hrs
@@ -107,21 +107,22 @@ staging = duckdb.sql(f"""
 runs_in_2025 = duckdb.sql('''
             SELECT *  
             FROM staging
-            where year(start_date_local)=2025
-            and type='Run'
+            WHERE year(start_date_local)=2025
+            AND type='Run'
            '''
    )
 
-# 1. How many miles did I run?
+#%%
+# How many miles did I run?
 duckdb.sql('''
-           with monthly as (
+           WITH monthly AS (
                 SELECT start_date_local_yyyy_mm,round(sum(distance_miles),2) as total_miles
                 FROM runs_in_2025
                 GROUP BY start_date_local_yyyy_mm
                 ORDER BY start_date_local_yyyy_mm
             )
-            , total as (
-                SELECT 'Grand total',round(sum(distance_miles),2) as total_miles
+            , total AS (
+                SELECT 'Grand total',round(sum(distance_miles),2) AS total_miles
                 FROM runs_in_2025
                 WHERE year(start_date_local)=2025
                 AND type='Run'
@@ -132,4 +133,47 @@ duckdb.sql('''
            '''
    )
 
+#%%
+# How many miles of each activity type did I do?
+duckdb.sql('''
+            SELECT type,round(sum(distance_miles),2) AS total_miles
+            FROM staging
+            WHERE year(start_date_local)=2025
+            GROUP BY type
+            HAVING total_miles>0
+            ORDER BY total_miles DESC
+           '''
+   )
+
+
 # %%
+# How many runs did I do?
+duckdb.sql('''
+            SELECT count(*) AS total_runs
+            FROM runs_in_2025
+            WHERE year(start_date_local)=2025
+            AND type='Run'
+           '''
+   )
+
+#%%
+# How many of each activity type did I do?
+duckdb.sql('''
+            SELECT type,count(*) AS activity_count
+            FROM staging
+            WHERE year(start_date_local)=2025
+            GROUP BY type
+            ORDER BY activity_count DESC
+           '''
+   )
+
+#%%
+# 3. How much elevation did I run?
+
+
+#%%
+# 4. How much total time did I run?
+
+
+#%%
+# 5. How much did these metrics change YoY?
