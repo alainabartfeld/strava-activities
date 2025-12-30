b4fd53f9d6249e9928a7d54dadd2e8c2ae3f9ec3
Alaina Bartfeld <alainabartfeld@gmail.com>
2025-12-30 18:03:05 -0500

    split the load and analysis into separate files

--- strava_analysis.py
+++ strava_analysis.py
@@ -1,25 +1,15 @@
 #%%
 import my_utils
+import data_load
 import duckdb
 import logging
 from jinja2 import Template
 
 #%%
-# Set up logging
-init_paths = my_utils.initialize_paths("logs", "strava_data", "strava_export")
-
-my_utils.setup_logging(init_paths["log_file_path"])
-logging.info("Starting Strava Analysis Pipeline")
-logging.info(f"Log deposited into: {init_paths['log_file_path']}")
-logging.info(f"Data deposited into: {init_paths['csv_file_path']}")
+# Get the data
+data = data_load.data
 
 #%%
-# Get the data
-data = my_utils.upload_data_to_duckdb(my_utils.download_data_from_strava(init_paths['csv_file_path']))
-logging.info("Strava Analysis Pipeline completed")
-
-
-# %%
 # Clean up the house
 miles_to_meters = 1609.34
 feet_to_meters = 0.3048
